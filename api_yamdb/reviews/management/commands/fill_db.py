1bd934141c5fe03bb141ebfa1fe3fc6d0ffff43c
Ларькин Михаил <jetpilot314@gmail.com>
2022-12-05 15:06:17 +0300

    CSV-filler refactor.

--- api_yamdb/reviews/management/commands/fill_db.py
+++ api_yamdb/reviews/management/commands/fill_db.py
@@ -1,108 +1,29 @@
-import pandas as pd
+import csv
+import os
 
 from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Comment, Genre, Review, Title, User
+from reviews.models import Category, Comment, Genre, Title, Review, User
 
 
 class Command(BaseCommand):
-    """Команда для загрузки данных из CSV"""
-
-    help = 'import data for Title, Genre, Category, Review, Comment, User'
+    help = "Closes the specified poll for voting"
+    data_folder = "static/data"
+    tables = (
+        {"model": User, "file": "users.csv"},
+        {"model": Genre, "file": "genre.csv"},
+        {"model": Category, "file": "category.csv"},
+        {"model": Title, "file": "titles.csv"},
+        {"model": Title.genre.through, "file": "genre_title.csv"},
+        {"model": Review, "file": "review.csv"},
+        {"model": Comment, "file": "comments.csv"},
+    )
 
     def handle(self, *args, **options):
-        df = pd.read_csv('static/data/category.csv', sep=',')
-        row_iter = df.iterrows()
-        objs = [
-            Category(
-                pk=row['id'],
-                name=row['name'],
-                slug=row['slug'],
-            )
-            for index, row in row_iter
-        ]
-
-        Category.objects.bulk_create(objs)
-
-        df = pd.read_csv('static/data/genre.csv', sep=',')
-        row_iter = df.iterrows()
-        objs = [
-            Genre(
-                pk=row['id'],
-                name=row['name'],
-                slug=row['slug'],
-            )
-            for index, row in row_iter
-        ]
-
-        Genre.objects.bulk_create(objs)
-
-        df = pd.read_csv('static/data/titles.csv', sep=',')
-        row_iter = df.iterrows()
-        objs = [
-            Title(
-                pk=row['id'],
-                name=row['name'],
-                year=row['year'],
-                category=Category.objects.get(id=row['category']),
-            )
-            for index, row in row_iter
-        ]
-
-        Title.objects.bulk_create(objs)
-
-        df = pd.read_csv('static/data/genre_title.csv', sep=',')
-        row_iter = df.iterrows()
-
-        for index, row in row_iter:
-            title = Title.objects.get(id=(row['title_id']))
-            genre = Genre.objects.get(id=(row['genre_id']))
-            title.genre.add(genre)
-
-        df = pd.read_csv('static/data/users.csv', sep=',')
-        row_iter = df.iterrows()
-        objs = [
-            User(
-                pk=row['id'],
-                username=row['username'],
-                email=row['email'],
-                role=row['role'],
-                bio=row['bio'],
-                first_name=['first_name'],
-                last_name=['last_name']
-            )
-            for index, row in row_iter
-        ]
-
-        User.objects.bulk_create(objs)
-
-        df = pd.read_csv('static/data/review.csv', sep=',')
-        row_iter = df.iterrows()
-        objs_review = [
-            Review(
-                pk=row['id'],
-                author=User.objects.get(id=row['author']),
-                text=row['text'],
-                pub_date=row['pub_date'],
-                score=row['score'],
-                title_id=row['title_id'],
-            )
-            for index, row in row_iter
-        ]
-
-        Review.objects.bulk_create(objs_review)
-
-        df = pd.read_csv('static/data/comments.csv', sep=',')
-        row_iter = df.iterrows()
-        objs = [
-            Comment(
-                pk=row['id'],
-                author=User.objects.get(id=row['author']),
-                text=row['text'],
-                pub_date=row['pub_date'],
-                review=Review.objects.get(id=row['review_id']),
-            )
-            for index, row in row_iter
-        ]
-
-        Comment.objects.bulk_create(objs)
+        for table in self.tables:
+            with open(
+                os.path.join(self.data_folder, table["file"]), encoding="UTF-8"
+            ) as file:
+                rows = csv.DictReader(file)
+                result = [table["model"](**row) for row in rows]
+            table["model"].objects.bulk_create(result)
